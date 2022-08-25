6a6e721490b964a723cb423bc6e04231877834c4
Luis <luismfalconi@gmail.com>
2022-08-25 10:27:43 -0300

    prueba branch

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <span className='m-2 font-bold'>Prueba commit branch</span>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
